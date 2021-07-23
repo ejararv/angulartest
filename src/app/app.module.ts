441b3c3829b4f13bb2701bca5bed700b71c843b8
Vladimir Reshetsko <44954340+ejararv@users.noreply.github.com>
2021-07-24 01:45:39 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireDatabaseModule} from '@angular/fire/database'
+import {environment} from './../environments/environment'
+
+
+import {InputFieldComponent} from './input-field/input-field.component'
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { ListComponent } from './list/list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputFieldComponent,
+    SearchBarComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyB0M2FXqFQS9mPDq7cKk1r_8veluws37lk",
+    authDomain: "angulartest-2655e.firebaseapp.com",
+    projectId: "angulartest-2655e",
+    storageBucket: "angulartest-2655e.appspot.com",
+    messagingSenderId: "288786768458",
+    appId: "1:288786768458:web:63e3d60abe29fc6a3bfcb3",
+    measurementId: "G-XC2G8DN73R"
+  }
 };
 
 /*
