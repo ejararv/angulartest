3ff2754f136003958ae5f7b7128ad456daebbbb9
Vladimir Reshetsko <44954340+ejararv@users.noreply.github.com>
2021-07-31 16:43:42 +0200

    Submit Angular-material form with IPayments interface and push to firebase

--- src/app/input-form/input-form.component.ts
+++ src/app/input-form/input-form.component.ts
@@ -3,17 +3,12 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { IPayments } from '../paymentslist/paymentslist.component';
 import { FirebaseService } from '../srvices/firebase.service';
 
-
-
-
 @Component({
   selector: 'app-input-form',
   templateUrl: './input-form.component.html',
   styleUrls: ['./input-form.component.css'],
 })
 export class InputFormComponent {
-
-
   payments: any = [];
   vat: number = 0;
   netto: number = 0;
@@ -22,7 +17,6 @@ export class InputFormComponent {
   description: string = '';
 
   paymentsForm = this.fb.group(
-
     {
       id: Date.now(),
       netto: this.netto,
@@ -31,32 +25,16 @@ export class InputFormComponent {
       contractor: this.contractor,
       description: this.description,
     }
-    
-  //  { 
-  //   netto:null,
-  //   position: [null, Validators.required],
-  //   contractor: [null, Validators.required],
-  //   description: null,
-  //   vat: [null, Validators.required],
-  //   // postalCode: [
-  //   //   null,
-  //   //   Validators.compose([
-  //   //     Validators.required,
-  //   //     Validators.minLength(5),
-  //   //     Validators.maxLength(5),
-  //   //   ]),
-  //   // ],
-  //   }
+
+   
   );
 
   vatOptions = [
     { name: '23', option: 23 },
     { name: '7', option: 7 },
-    
   ];
 
   constructor(private fb: FormBuilder, private storeService: FirebaseService) {}
-
 
   addPayment() {
     const payment: IPayments = {
@@ -66,19 +44,13 @@ export class InputFormComponent {
       position: this.position,
       contractor: this.contractor,
       description: this.description,
-    }
-    this.storeService.addPayment(payment)
+    };
   }
-    
-  
 
-   
-  
   onSubmit(): void {
-    
-    this.addPayment()
+    const payment: IPayments = this.paymentsForm.getRawValue();
+
+    this.storeService.addPayment(payment);
     alert('Thanks!');
   }
-
-  
 }
